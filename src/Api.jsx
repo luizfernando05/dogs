d67ffcca6e50aaf36b842a6562765c8cda040e1d
luizfernando05 <luiz.fernando.lf2016@outlook.com>
2024-05-04 15:25:30 -0300

    fix: corrigindo erro na api

--- src/Api.jsx
+++ src/Api.jsx
@@ -4,48 +4,48 @@ export function TOKEN_POST(body) {
     return {
         url: API_URL + '/jwt-auth/v1/token',
         options: {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'aplication/json',
-            },
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
         },
         body: JSON.stringify(body),
-    }
+        },
+    };
 }
 
 export function TOKEN_VALIDATE_POST(token) {
     return {
         url: API_URL + '/jwt-auth/v1/token/validate',
         options: {
-            method: 'POST',
-            headers: {
-                Autorization: 'Bearer ' + token,
-            },
-        }
-    }
+        method: 'POST',
+        headers: {
+            Authorization: 'Bearer ' + token,
+        },
+        },
+    };
 }
 
 export function USER_GET(token) {
     return {
         url: API_URL + '/api/user',
         options: {
-            method: 'GET',
-            headers: {
-                Autorization: 'Bearer ' + token,
-            },
+        method: 'GET',
+        headers: {
+            Authorization: 'Bearer ' + token,
         },
-    }
+        },
+    };
 }
 
-export function USER_POST (body) {
+export function USER_POST(body) {
     return {
         url: API_URL + '/api/user',
         options: {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'aplication/json',
-            },
-            body: JSON.stringify(body),
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(body),
         },
     };
 }
