c98dcb97cd73fe75f2f9c99a712057ef2131e9da
luizfernando05 <luiz.fernando.lf2016@outlook.com>
2024-05-06 14:44:36 -0300

    feat: inserindo estrutura inicial do modal

--- src/Components/Feed/FeedModal.jsx
+++ src/Components/Feed/FeedModal.jsx
@@ -1,8 +1,20 @@
 import React from 'react';
+import styles from './FeedModal.module.css';
+import useFetch from '../../Hooks/useFetch';
+import { PHOTO_GET } from '../../Api';
 
-const FeedModal = () => {
+const FeedModal = ({ photo }) => {
+  const { data, error, loading, request } = useFetch();
+
+  React.useEffect(() => {
+    const { url, options } = PHOTO_GET(photo.id);
+    request(url, options);
+  }, [photo, request]);
+
   return (
-    <div>FeedModal</div>
+    <div className={styles.modal}>
+      <img src={photo.src}></img>
+    </div>
   )
 }
 
