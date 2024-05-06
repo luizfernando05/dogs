f147b9c21d72a4cc09da6661a01ee1dd80074adf
luizfernando05 <luiz.fernando.lf2016@outlook.com>
2024-05-06 13:42:05 -0300

    feat: inserindo pagina para postagem de foto

--- src/Hooks/useForm.jsx
+++ src/Hooks/useForm.jsx
@@ -8,6 +8,10 @@ const types = {
     password: {
         regex: /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z]{8,}$/,
         message: 'A senha precisa ter 1 caracter maiúsculo, 1 minúsculo e 1 caractere especial. Com no mínimo 8 dígitos.'
+    },
+    number: {
+        regex: /^\d+$/,
+        message: 'Utilize apenas números.',
     }
 }
 
